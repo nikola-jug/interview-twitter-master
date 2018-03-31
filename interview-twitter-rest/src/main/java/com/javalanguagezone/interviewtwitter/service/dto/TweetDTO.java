1f27451305e3aedff7ee5d57bafd178be621f3bc
Nikola Jug <nikola.jug@ecx.io>
2018-03-31 15:03:50 +0200

    Initial commit

---
+++ interview-twitter-rest/src/main/java/com/javalanguagezone/interviewtwitter/service/dto/TweetDTO.java
@@ -0,0 +1,21 @@
+package com.javalanguagezone.interviewtwitter.service.dto;
+
+import com.javalanguagezone.interviewtwitter.domain.Tweet;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@NoArgsConstructor(access = PRIVATE)
+public class TweetDTO {
+  private Long id;
+  private String content;
+  private String author;
+
+  public TweetDTO(Tweet tweet) {
+    this.id = tweet.getId();
+    this.content = tweet.getContent();
+    this.author = tweet.getAuthor().getUsername();
+  }
+}
